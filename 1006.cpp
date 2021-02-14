a3c3fb8d74c017e76161aa3cbe42be21426e64cf
Md. Ibrahim Mamun <67334936+mamun-104@users.noreply.github.com>
2021-02-15 01:03:08 +0600

    Add files via upload

---
+++ 1006.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+int main()
+{
+    double a,b,c, MEDIA;
+    scanf("%lf%lf%lf",&a,&b,&c);
+
+    MEDIA = (a*2 + b*3 + c*5)/(2+3+5);
+    printf("MEDIA = %.1lf\n",MEDIA);
+
+
+    return 0;
+}
+
+
+
+
